af898dc4286fa9e4acf473f1f3a33937853a5a2a
Bridge Dudley <elbridge@umich.edu>
2020-03-04 13:01:19 -0500

    semantic working cleanly

--- react-client/src/Components/NavBar.jsx
+++ react-client/src/Components/NavBar.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
 
-class NavBar extends Component {
-  render() {
-    return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
-    )
-  }
-}
+const panes = [
+  { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+  { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+  { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+]
+
+const NavBar = () => <Tab panes={panes} />
 
 export default NavBar;
