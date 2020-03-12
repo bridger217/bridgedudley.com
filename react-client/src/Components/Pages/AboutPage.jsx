0cba2f2897c2c5b9fa5708674d4b67595c72c3f5
Bridge Dudley <elbridge@umich.edu>
2020-03-12 12:44:17 -0400

    update

--- react-client/src/Components/Pages/AboutPage.jsx
+++ react-client/src/Components/Pages/AboutPage.jsx
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 import AboutCard from './About/AboutCard';
-import background from '../../../static/images/background.png';
+import background from '../../../static/images/space-background.jpg';
 
 class AboutPage extends Component {
   render() {
     return (
       <Container
         style={{
-          width: '100vw',
-          height: '100vh',
-          backgroundSize: '100% 100%',
-          backgroundRepeat: 'no-repeat',
-          position: 'relative',
-          marginBottom: '-100px',
-          opacity: 0.5,
-          backgroundImage: `url(${background})`,
-          marginBottom: 0,
-      }}
+            width: '100vw',
+            height: '100vh',
+            backgroundSize: '100% 100%',
+            backgroundRepeat: 'no-repeat',
+            position: 'relative',
+            marginBottom: '-100px',
+            backgroundImage: `url(${background})`,
+            marginBottom: 0,
+        }}
       >
-        <div class="about-card-wrapper" style={{opacity: '1.0'}}>
+        <div class="about-card-wrapper">
           <div class="about-card-inner">
             <AboutCard />
           </div>
