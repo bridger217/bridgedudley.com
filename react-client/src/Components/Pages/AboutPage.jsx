db96b5f9fa6a2a2af9dc64aaf248bf9f4348823b
Bridge Dudley <elbridge@umich.edu>
2020-03-12 12:03:39 -0400

    background better, need to fix opacity of divs over top

--- react-client/src/Components/Pages/AboutPage.jsx
+++ react-client/src/Components/Pages/AboutPage.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import AboutCard from './About/AboutCard'
+import { Container } from 'semantic-ui-react';
+import AboutCard from './About/AboutCard';
+import background from '../../../static/images/background.png';
 
 class AboutPage extends Component {
   render() {
     return (
-      <div>
-        <div id="backgroundimage" />
-        <div class="about-card-wrapper">
+      <Container
+        style={{
+          width: '100vw',
+          height: '100vh',
+          backgroundSize: '100% 100%',
+          backgroundRepeat: 'no-repeat',
+          position: 'relative',
+          marginBottom: '-100px',
+          opacity: 0.5,
+          backgroundImage: `url(${background})`,
+          marginBottom: 0,
+      }}
+      >
+        <div class="about-card-wrapper" style={{opacity: '1.0'}}>
           <div class="about-card-inner">
-            <AboutCard />
             <AboutCard />
           </div>
         </div>
-      </div>
+      </Container>
     )
   }
 }
